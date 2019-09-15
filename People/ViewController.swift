a390e73ef8976c1699ece82332139e275b8b9889
Yugantar Jain <yugantar8jain@gmail.com>
2019-09-14 20:14:52 -0400

    Initial Commit

---
+++ People/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  People
+//
+//  Created by yugantar jain on 14/09/19.
+//  Copyright © 2019 yugantar jain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
